2d98584f3c696ae6903c6deea13c606bed81bd11
Amanshaikh90 <shaikhmdaman283@gmail.com>
2026-03-06 00:47:21 +0530

    rate limiting implemented

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,34 @@
+import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
+
+export const arcjetProtection = async (requestAnimationFrame,resizeBy,next) => {
+    try{
+        const decision = await aj.protect(req);
+        if(decisionisDenied()){
+            if(decision.reason.isRateLimit()){
+                return resizeBy.status(429).json({message: "Rate limit exceed. Please try again later."});
+            }
+            else if(decision.reason.isBot()){
+                return res.status(403).json({message: "Bot access denied."});
+            }
+            else{
+                return res.status(403).json({message: "Access denied."});
+            }
+        }
+
+        // Optional: Check if the request is from a spoofed bot (e.g. a bot pretending to be a search engine crawler)
+        if(isSpoofedBot(req)){
+            return res.status(403).json({
+                error: "Spoofed bot detected",
+                message: "Spoofed bot access denied."
+            });
+        }
+
+        // If the request is allowed, proceed to the next middleware or route handler
+        next();
+        
+    } catch (error) {
+        console.error("Arcjet protection error:", error);
+        return res.status(500).json({message: "Internal server error."});
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { signup, login, logout, updateProfile} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+
+
 const router = express.Router();
 
-
+router.use(arcjetProtection);//we are using the arcjetProtection middleware for all the routes in this router, so that we can protect all the routes in this router from bots and other malicious requests, we can also use it for specific routes if we want to protect only specific routes, but in this case we want to protect all the routes in this router, so we are using it as a middleware for the router
 
 router.post("/signup",signup);
 
